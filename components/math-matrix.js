5f1e256fd35546c9532892b28f5422b7a102f1ed
Aaron Dekker <adekker@mgmarketing.co.nz>
2019-01-18 10:51:22 +1300

    when styling an object works :)

--- components/math-matrix.js
+++ components/math-matrix.js
@@ -1,36 +1,52 @@
 Vue.component("math-matrix", {
   props: {
-    initEntries: Array
+    initEntries: Array,
+    initPosition: Array
   },
   data: function () {
     return {
-      entries: this.initEntries || [[1,0,0],[0,1,0],[0,0,1]],
-      shouldUpdate: false
+      entries: [[1,0,0],[0,1,0],[0,0,1]],
+      position: [0,0],
+      styleObj: {
+        position: 'absolute',
+        // we can either orientate things from the bottom left, or the top right.
+        // that or centre everything?
+        left: `0px`,
+        bottom: `0px`,
+      }
+      //shouldUpdate: false
+    }
+  },
+  created: function () {
+    if (this.initEntries) {
+      this.entries = this.initEntries 
+    }
+    if (this.initPosition) {
+      this.position = this.initPosition
+      this.styleObj.left = `${this.initPosition[0]}px`
+      this.styleObj.bottom   = `${this.initPosition[1]}px`
     }
   },
   methods: {
     newEntry: function () {
       try {
         this.entries[i][j] = value
-        this.shouldUpdate = true
+        //this.shouldUpdate = true
       } catch (error) {
         console.log(error);
       }
-    },
-    newMatrix: function () {
-      this.entries = matrix
     }
   },
   computed: {
     generateTemplate: function () {
-      return `<table>
+      return `<table v-bind:style="styleObj">
       <tr v-for="(row, i) in entries">
         <td v-for="(item, j) in row"> {{ entries[i][j] }} </td>
       </tr>
     </table>`
     }
   },
-  template: `<table>
+  template: `<table v-bind:style="styleObj">
   <tr v-for="(row, i) in entries">
     <td v-for="(item, j) in row"> {{ entries[i][j] }} </td>
   </tr>
--- main.js
+++ main.js
@@ -1,16 +1,15 @@
 // Got a library library from the internet.
 // console.log(math);
 
-let x = math.matrix([[1,0],[0,1]])
-console.log(x);
-console.log(x.toString());
+//let x = math.matrix([[1,0],[0,1]])
 
-vueData = {
+mainData = {
   message: 'hello world',
   // An array of objects which describe all the matrices
   matrices: [
     {
       id: "test1",
+      position: [100, 100],
       entries: [[1,2,3],[4,5,6],[7,8,9]]
     },
     {
@@ -23,20 +22,43 @@ vueData = {
   ]
 }
 
+function createMatrix(event) {
+  //console.log(event);
+  mainData.matrices.push({
+    id:"testing",
+    position: [100, 100],
+    entries: [[5,5,5],[4,4,4],[3,2,1]]
+  })
+}
+
 const TheMatrix = new Vue({
   el: '#TheMatrix',
   data: function () {
-    return vueData
+    return mainData
+  },
+  methods: {
+    createMatrix: function () {
+      // 
+    }
   },
   template: `<div><p>{{message}}</p>
   <math-matrix v-for="(matrix, index) in matrices"
   v-bind:key="index"
   v-bind:id="matrix.id"
-  v-bind:initEntries="matrix.entries">
+  v-bind:initEntries="matrix.entries"
+  v-bind:initPosition="matrix.position">
   </math-matrix>
 </div>`
 })
 
+//console.log(TheMatrix);
+//console.log(TheMatrix.$refs);
+
+window.onload = function () {
+  console.log("hello again, again");
+  //mainData.matrices.push({id:"test4",entries:[[3,3],[2,1]]})
+}
+document.onclick = createMatrix
 /*
 window.customElements.define('math-matrix', class extends HTMLElement {
   constructor() {
