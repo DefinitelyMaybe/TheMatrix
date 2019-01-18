357eed2640a24d539dcd8a910099a94f49ea235a
Aaron Dekker <adekker@mgmarketing.co.nz>
2019-01-19 09:43:29 +1300

    removed old custom element code.

--- main.js
+++ main.js
@@ -59,46 +59,5 @@ window.onload = function () {
   console.log("hello again, again");
   //mainData.matrices.push({id:"test4",entries:[[3,3],[2,1]]})
 }
+
 document.onclick = createMatrix
-/*
-window.customElements.define('math-matrix', class extends HTMLElement {
-  constructor() {
-    super();
-    // Attach the shadowDom
-    this.shadow = this.attachShadow({mode: 'open'});
-
-    // Create the html that we're going to attach to the shadowDom
-    this.table = document.createElement('table')
-    this.entries = [[1]]
-    for (let i = 0; i < this.entries.length; i++) {
-      let row = document.createElement('tr')
-      for (let j = 0; j < this.entries[i].length; j++) {
-        let x = document.createElement('td')
-        x.innerHTML = this.entries[i][j];
-        row.appendChild(x)
-      }
-      this.table.appendChild(row)
-    }
-
-    this.style = document.createElement('style')
-    this.style.textContent = `table {
-      background: white;
-    }`
-
-    // Attach the elements to the shadowDom
-    this.shadow.appendChild(this.style)
-    this.shadow.appendChild(this.table)
-  }
-});
-
-//const shadowDom = document.body.attachShadow({mode:'open'})
-
-//x = document.createElement('math-matrix')
-
-//shadowDom.appendChild(x)
-
-
-console.log(math.add(x, x).toString());
-
-//document.body.appendChild(document.createElement('math-matrix'))
-*/
