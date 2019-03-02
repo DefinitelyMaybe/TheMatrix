92418900cc9ed2e1178dbe24716f77d136f10a63
Rawr <rekked.noraa@gmail.com>
2019-03-02 18:24:33 +1300

    created defaults and hooked into parent form.

--- components/form-function.js
+++ components/form-function.js
@@ -1,10 +1,10 @@
 Vue.component("form-function", {
   data: function () {
     return {
+      mathq: '',
       name: "f",
-      initWithFunction: false,
-      writeOutFunction: false,
-      latexInitFunction: false
+      latex: "x+1",
+      importFunc: false
     }
   },
   created: function () {
@@ -15,18 +15,40 @@ Vue.component("form-function", {
       this.styleObj.top = this.initData.position[1]
     }
   },
+  mounted () {
+    // the MQ variable is defined in main.js and is equal to: MathQuill.getInterface(2);
+    // TODO: this is throwing the two error messages at the start. Why?
+    this.mathq =  MQ.MathField(this.$refs.quillspan, {
+      handlers: {
+        edit: this.spanEdit
+      }
+    })
+    this.mathq.latex(this.latex)
+  },
   methods: {
     //form specific
+    finishForm: function () {
+      this.$parent.finishForm({
+        name: this.name,
+        latex: this.latex
+      })
+    },
+    spanEdit: function () {
+      this.latex = this.mathq.latex()
+    }
   },
   template: `<form onsubmit="return false">
-  <label>What would you like to name your function?</label>
-  <input type="text" v-model="name"></input>
-  <label>Would you like to name your function?</label>
-  <input type="checkbox" v-model="initWithFunction"></input>
-  <template v-if="initWithFunction">
-    <label>Would you like to write it yourself or paste in some latex?</label>
-    <label>Write it out.</label><input type="checkbox" v-model="writeOutFunction"></input>
-    <label>Paste in latex.</label><input type="checkbox" v-model="latexInitFunction"></input>
-  </template>
+  <label>Name:</label>
+  <input type="text" v-model="name"></input><br>
+  <label v-show="!importFunc">Function:</label>
+  <span ref="quillspan"
+  v-bind:class="{formQuill: true}"
+  v-show="!importFunc"></span><br v-show="!importFunc">
+  <label>Import latex</label>
+  <input type="checkbox" v-model="importFunc"></input><br>
+  <template v-if="importFunc">
+    <textarea v-model="latex"></textarea>
+  </template><br>
+  <button v-on:click="finishForm">Finish</button>
 </form>`,
 })
