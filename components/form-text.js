0f01b39bd4905576525b4e97c76985abda2e779e
Rawr <rekked.noraa@gmail.com>
2019-03-04 21:18:49 +1300

    Text is nice and simple. done.

--- components/form-text.js
+++ components/form-text.js
@@ -1,22 +1,11 @@
 Vue.component("form-text", {
-  data: function () {
-    return {
-      name: "f",
-    }
-  },
-  created: function () {
-    if (this.initData) {
-      //console.log(this.initData);
-      this.formData = this.initData.formData
-      this.styleObj.left = this.initData.position[0]
-      this.styleObj.top = this.initData.position[1]
-    }
-  },
   methods: {
     //form specific
+    finishForm: function () {
+      this.$parent.finishForm({})
+    }
   },
   template: `<form onsubmit="return false">
-  <label for="name">What would you like to name your function?</label>
-  <input type="text" v-model="name"></input>
+  <button v-on:click="finishForm">Finish</button>
 </form>`,
 })
