c5e5de4a70d76028ac3ca2730fe24d5723d32538
Rawr <rekked.noraa@gmail.com>
2019-03-04 21:18:25 +1300

    table form fleshed out. Will create table but incomplete.

--- components/form-table.js
+++ components/form-table.js
@@ -1,7 +1,15 @@
 Vue.component("form-table", {
   data: function () {
     return {
-      name: "f",
+      inputVarCount: 1,
+      inputNames: ["x"],
+      outputFuncCount: 1,
+      outputNames: ["f"],
+
+      rowCount: 5,
+      varStep: [1],
+      inputRanges: [[1], [2], [3], [4], [5]],
+      outputValues: [["?"], ["?"], ["?"], ["?"], ["?"]],
     }
   },
   created: function () {
@@ -14,9 +22,27 @@ Vue.component("form-table", {
   },
   methods: {
     //form specific
+    finishForm: function () {
+      this.$parent.finishForm({
+        inputHeaders: this.inputNames,
+        inputTable: this.inputRanges,
+        outputHeaders: this.outputNames,
+        outputTable: this.outputValues,
+      })
+    }
   },
   template: `<form onsubmit="return false">
-  <label for="name">What would you like to name your function?</label>
-  <input type="text" v-model="name"></input>
+  <label>How many input variables would you like:</label>
+  <input type="number" v-model="inputVarCount"></input><br>
+  <label>How many output functions would you like:</label>
+  <input type="number" v-model="outputFuncCount"></input><br>
+  
+  <label>Name the variables:</label><br>
+  <template v-for="index in inputNames">
+    <input type="text" v-model="inputNames[index]" v-bind:key="index"></input><br>
+  </template>
+  <label>How many rows:</label>
+  <input type="number" v-model="rowCount"></input><br>
+  <button v-on:click="finishForm">Finish</button>
 </form>`,
 })
