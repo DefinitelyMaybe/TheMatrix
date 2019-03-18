aa3646ee9bce5bfbe31236e4475a528c63a72630
Aaron Dekker <adek841@UoA.auckland.ac.nz>
2019-03-18 15:49:59 +1300

    encapsulated functionality

---
+++ components/mixins/moveable.js
@@ -0,0 +1,30 @@
+mixin_moveable = {
+  data: function () {
+    return {
+      objStyle: {
+        'position': 'absolute',
+        'left': '0px',
+        'top': '0px'
+      }
+    }
+  },
+  created: function () {
+    if (this.initData) {
+      this.objStyle.left = this.initData.position[0]
+      this.objStyle.top = this.initData.position[1]
+    }
+  },
+  methods: {
+    onDragEnd: function (event) {
+      let x = event.x - this.dragOffsetX
+      let y = event.y - this.dragOffsetY
+      this.objStyle.left = `${x}px`
+      this.objStyle.top = `${y}px`
+    },
+    onDragStart: function (event) {
+      this.onClick()
+      this.dragOffsetX = event.offsetX
+      this.dragOffsetY = event.offsetY
+    },
+  },
+}
