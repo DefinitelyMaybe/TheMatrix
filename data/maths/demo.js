649853aff74bc13145efb11a93bdb59ea9e2d772
Rawr <rekked.noraa@gmail.com>
2019-03-25 17:22:56 +1300

    created a demo scene file.

---
+++ data/maths/demo.js
@@ -0,0 +1,56 @@
+DATA_objects = [{
+  "name": "z",
+  "value": 5,
+  "position": ["537px", "59px"],
+  "type": "object-variable",
+  "id": "0"
+}, {
+  "name": "f",
+  "latex": "\\frac{4}{\\pi}\\sin\\left(x\\cdot\\pi\\right)+\\frac{4}{3\\pi}\\sin\\left(3x\\cdot\\pi\\right)+\\frac{4}{5\\pi}\\sin\\left(5x\\cdot\\pi\\right)+\\frac{4}{7\\pi}\\sin\\left(7x\\cdot\\pi\\right)",
+  "position": ["71px", "314px"],
+  "type": "object-function",
+  "id": "1"
+}, {
+  "name": "g",
+  "latex": "10z^{2-x}",
+  "position": ["516px", "160px"],
+  "type": "object-function",
+  "id": "2"
+}, {
+  "inputHeaders": ["x", "y"],
+  "inputTable": [
+    ["1", "2"],
+    ["2", "3"],
+    ["3", "4"],
+    ["4", "5"],
+    ["5", "6"]
+  ],
+  "outputHeaders": ["g"],
+  "outputTable": [
+    ["50"],
+    ["10"],
+    ["2"],
+    ["0.4"],
+    ["0.08"]
+  ],
+  "position": ["675px", "38px"],
+  "type": "object-table",
+  "id": "3"
+}, {
+  "position": ["30px", "383px"],
+  "width": 700,
+  "height": 500,
+  "type": "object-graph",
+  "id": "4",
+  "xaxis": "x",
+  "yaxis": "f",
+  "xrange": ["0", "4"],
+  "yrange": ["-2", "2"]
+}, {
+  "value": "The web maths IDE.\n\nRight click anywhere to see different options. Drag things around as you wish.\n\nThere's variables, functions, tables and some basic graphs. Tensorflow is coming soon :)\n\nNote: Not all functions will work as I've needed to parse latex for evaluation. (no logs sorry!)",
+  "width": "436px",
+  "height": "259px",
+  "position": ["23px", "15px"],
+  "type": "object-text",
+  "id": "5"
+}]
