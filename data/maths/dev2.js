65b4abe3cc1651b773389933f5cc4e351e1263a9
Aaron Dekker <adek841@UoA.auckland.ac.nz>
2019-04-03 18:17:58 +1300

    added new potential format

---
+++ data/maths/dev2.js
@@ -0,0 +1,95 @@
+{
+  "filename" = "test"
+  "pages" = [
+    {
+      "name": "Dev1Page",
+      "position": 0,
+      "sceneObjects": [{
+        "name": "z",
+        "value": 5,
+        "position": ["188px", "208px"],
+        "type": "object-variable",
+      }, {
+        "name": "f",
+        "latex": "2\\cos\\left(\\frac{x}{3}\\right)",
+        "position": ["384px", "88px"],
+        "type": "object-function",
+      }, {
+        "name": "g",
+        "latex": "10z^{2-x}",
+        "position": ["183px", "79px"],
+        "type": "object-function",
+      }, {
+        "inputHeaders": ["x", "y"],
+        "inputTable": [
+          [1, 2],
+          [2, 3],
+          [3, 4],
+          [4, 5],
+          [5, 6]
+        ],
+        "outputHeaders": ["f", "g"],
+        "outputTable": [
+          ["1.89", "50"],
+          ["1.572", "10"],
+          ["1.081", "2"],
+          ["0.4705", "0.4"],
+          ["-0.1914", "0.08"]
+        ],
+        "position": ["575px", "94px"],
+        "type": "object-function-table",
+      }, {
+        "headers": ["x", "y"],
+        "table": [
+          [1, 2],
+          [2, 3],
+          [3, 4],
+          [4, 5],
+          [5, 6]
+        ],
+        "position": ["700px", "100px"],
+        "type": "object-table",
+      }, {
+        "position": ["65px", "335px"],
+        "width": 700,
+        "height": 500,
+        "type": "object-graph",
+        "xaxis": "x",
+        "yaxis": "f",
+        "xrange": [-10, 10],
+        "yrange": [-10, 10]
+      }]
+    },
+    {
+      "name": "Dev2Page",
+      "position": 1,
+      "sceneObjects": [{
+        "name": "z",
+        "value": 5,
+        "position": ["188px", "208px"],
+        "type": "object-variable",
+      }, {
+        "name": "f",
+        "latex": "2\\cos\\left(\\frac{x}{3}\\right)",
+        "position": ["384px", "88px"],
+        "type": "object-function",
+      }, {
+        "name": "g",
+        "latex": "10z^{2-x}",
+        "position": ["183px", "79px"],
+        "type": "object-function",
+      }, {
+        "headers": ["x", "y"],
+        "table": [
+          [1, 2],
+          [2, 3],
+          [3, 4],
+          [4, 5],
+          [5, 6]
+        ],
+        "position": ["700px", "100px"],
+        "type": "object-table",
+      }]
+    }
+  ]
+}
