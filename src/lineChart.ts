97506372a8256ee7bedfe010adfc2a87e237e7c6
DefinitelyMaybe <rekked.noraa@gmail.com>
2020-10-23 12:23:27 +1300

    deno fmt

--- src/lineChart.ts
+++ src/lineChart.ts
@@ -1,52 +1,72 @@
 // graph library
-import * as d3 from "https://deno.land/x/d3_4_deno@v6.2.0.1/src/mod.js";
+import * as d3 from "https://deno.land/x/d3_4_deno@v6.2.0.6/src/mod.js";
 // https://observablehq.com/@d3/line-chart
 // https://www.d3-graph-gallery.com/graph/line_basic.html
 
-console.log(d3);
+var margin = { top: 10, right: 30, bottom: 30, left: 60 },
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
 
-let width = 500
-let height = 500
-
-const margin = {
-  top:20,
-  bottom: 30,
-  right:30,
-  left:40
-}
-
-const svg = d3.select("output#graph")
+// append the svg object to the body of the page
+var svg = d3.select("output#graph")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.csv("url", (d) => {
-  return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
-}, (data)=> {
-  var x = d3.scaleTime()
-      .domain(d3.extent(data, function(d) { return d.date; }))
-      .range([ 0, width ]);
-    svg.append("g")
-      .attr("transform", "translate(0," + height + ")")
-      .call(d3.axisBottom(x));
+const data = d3.csv(
+  "https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/3_TwoNumOrdered_comma.csv",
+);
+console.log(data);
+//Read the data
+// d3.csv(
+//   ,
+//   // When reading the csv, I must format variables:
+//   function (d:any) {
+//     console.log(d);
+//     return { date: d3.timeParse("%Y-%m-%d")(d.date), value: d.value };
+//   },
+//   // Now I can use this dataset:
+//   function (data) {
+//     console.log(data);
+//     // Add X axis --> it is a date format
+//     var x = d3.scaleTime()
+//       .domain(d3.extent(data, function (d) {
+//         return d.date;
+//       }))
+//       .range([0, width]);
+//     svg.append("g")
+//       .attr("transform", "translate(0," + height + ")")
+//       .call(d3.axisBottom(x));
 
-    // Add Y axis
-    var y = d3.scaleLinear()
-      .domain([0, d3.max(data, function(d) { return +d.value; })])
-      .range([ height, 0 ]);
-    svg.append("g")
-      .call(d3.axisLeft(y));
+//     // Add Y axis
+//     var y = d3.scaleLinear()
+//       .domain([
+//         0,
+//         d3.max(data, function (d) {
+//           return +d.value;
+//         }),
+//       ])
+//       .range([height, 0]);
+//     svg.append("g")
+//       .call(d3.axisLeft(y));
 
-    // Add the line
-    svg.append("path")
-      .datum(data)
-      .attr("fill", "none")
-      .attr("stroke", "steelblue")
-      .attr("stroke-width", 1.5)
-      .attr("d", d3.line()
-        .x(function(d) { return x(d.date) })
-        .y(function(d) { return y(d.value) })
-        )
-})
+//     // Add the line
+//     svg.append("path")
+//       .datum(data)
+//       .attr("fill", "none")
+//       .attr("stroke", "steelblue")
+//       .attr("stroke-width", 1.5)
+//       .attr(
+//         "d",
+//         d3.line()
+//           .x(function (d) {
+//             return x(d.date);
+//           })
+//           .y(function (d) {
+//             return y(d.value);
+//           }),
+//       );
+//   },
+// );
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,36 @@
 /// <reference lib="dom" />
 
+import "./lineChart.ts";
 // variables used in calculation
-let yearlyInterestRate = 0.0
-let initialAmount = 0.0
-let incuredInterestRate = 0.0
-let interestCalculationsPerYear = 0.0
-let totalYears = 0.0
-let totalIncuredInterestEvents = 0.0
-let extra = 0.0
-let resultValue = 0.0
+let yearlyInterestRate = 0.0;
+let initialAmount = 0.0;
+let incuredInterestRate = 0.0;
+let interestCalculationsPerYear = 0.0;
+let totalYears = 0.0;
+let totalIncuredInterestEvents = 0.0;
+let extra = 0.0;
+let resultValue = 0.0;
 
 // All inputs
-const yearlyInterestRateInput = document.querySelector("input#interestRate")! as HTMLInputElement
-const interestPerInput = document.querySelector("select#peroid")! as HTMLSelectElement
-const initialAmountInput = document.querySelector("input#initialAmount")! as HTMLInputElement
-const extraContributionsInput = document.querySelector("input#extraContributions")! as HTMLInputElement
-const extraAmountInput = document.querySelector("input#extraAmount")! as HTMLInputElement
-const totalYearsInput = document.querySelector("input#term")! as HTMLInputElement
-const totalYearsInputNumber = document.querySelector("label#termNumberLabel")!
+const yearlyInterestRateInput = document.querySelector(
+  "input#interestRate",
+)! as HTMLInputElement;
+const interestPerInput = document.querySelector(
+  "select#peroid",
+)! as HTMLSelectElement;
+const initialAmountInput = document.querySelector(
+  "input#initialAmount",
+)! as HTMLInputElement;
+const extraContributionsInput = document.querySelector(
+  "input#extraContributions",
+)! as HTMLInputElement;
+const extraAmountInput = document.querySelector(
+  "input#extraAmount",
+)! as HTMLInputElement;
+const totalYearsInput = document.querySelector(
+  "input#term",
+)! as HTMLInputElement;
+const totalYearsInputNumber = document.querySelector("label#termNumberLabel")!;
 // const graph = c3.generate({
 //   bindto: 'output#graph',
 //   data: {
@@ -27,85 +40,86 @@ const totalYearsInputNumber = document.querySelector("label#termNumberLabel")!
 //     ]
 //   }
 // })
-const resultOutput = document.querySelector("output#result")!
+const resultOutput = document.querySelector("output#result")!;
 
 // functionality
 function compute() {
   // https://money.stackexchange.com/questions/16507/calculate-future-value-with-recurring-deposits/26187
-  let newValue = NaN
-  const changeFormula = extraContributionsInput.checked
+  let newValue = NaN;
+  const changeFormula = extraContributionsInput.checked;
   if (!changeFormula) {
-    newValue = initialAmount * (1 + incuredInterestRate) ** totalIncuredInterestEvents
+    newValue = initialAmount *
+      (1 + incuredInterestRate) ** totalIncuredInterestEvents;
   } else {
-    const twoT = (1 + incuredInterestRate) ** (interestCalculationsPerYear * totalIncuredInterestEvents)
-    newValue = initialAmount * twoT + extra * ((twoT-1)/(incuredInterestRate)) * (1 + incuredInterestRate)
+    const twoT = (1 + incuredInterestRate) **
+      (interestCalculationsPerYear * totalIncuredInterestEvents);
+    newValue = initialAmount * twoT +
+      extra * ((twoT - 1) / (incuredInterestRate)) * (1 + incuredInterestRate);
   }
   // checking for valid results
   if (newValue) {
     // formatting
-    resultValue = Math.round(newValue * 100 ) / 100
+    resultValue = Math.round(newValue * 100) / 100;
     // updating html
-    resultOutput.innerHTML = `result: $${resultValue}`
+    resultOutput.innerHTML = `result: $${resultValue}`;
   }
 }
 
 // attach listeners to html elements
 yearlyInterestRateInput.addEventListener("change", () => {
   yearlyInterestRate = parseFloat(yearlyInterestRateInput.value);
-  const max = parseFloat(yearlyInterestRateInput.getAttribute("max")!)
-  const min = parseFloat(yearlyInterestRateInput.getAttribute("min")!)
+  const max = parseFloat(yearlyInterestRateInput.getAttribute("max")!);
+  const min = parseFloat(yearlyInterestRateInput.getAttribute("min")!);
   if (yearlyInterestRate > max) {
-    yearlyInterestRate = max
-    yearlyInterestRateInput.value = yearlyInterestRate.toString()
+    yearlyInterestRate = max;
+    yearlyInterestRateInput.value = yearlyInterestRate.toString();
   } else if (yearlyInterestRate < min) {
-    yearlyInterestRate = min
-    yearlyInterestRateInput.value = yearlyInterestRate.toString()
+    yearlyInterestRate = min;
+    yearlyInterestRateInput.value = yearlyInterestRate.toString();
   }
   // convert to [0, 1.0] range
-  yearlyInterestRate = yearlyInterestRate / 100
-  incuredInterestRate = yearlyInterestRate / interestCalculationsPerYear
-})
+  yearlyInterestRate = yearlyInterestRate / 100;
+  incuredInterestRate = yearlyInterestRate / interestCalculationsPerYear;
+});
 
 interestPerInput.addEventListener("change", () => {
-  interestCalculationsPerYear = parseInt(interestPerInput.value)
-  incuredInterestRate = yearlyInterestRate / interestCalculationsPerYear
-  totalIncuredInterestEvents = interestCalculationsPerYear * totalYears
-})
+  interestCalculationsPerYear = parseInt(interestPerInput.value);
+  incuredInterestRate = yearlyInterestRate / interestCalculationsPerYear;
+  totalIncuredInterestEvents = interestCalculationsPerYear * totalYears;
+});
 
 initialAmountInput.addEventListener("change", () => {
-  initialAmount = parseFloat(initialAmountInput.value)
-})
+  initialAmount = parseFloat(initialAmountInput.value);
+});
 
 extraContributionsInput.addEventListener("click", (e: any) => {
   if (e.target.checked) {
-    extraAmountInput.removeAttribute("hidden")
+    extraAmountInput.removeAttribute("hidden");
   } else {
-    extraAmountInput.setAttribute("hidden", "true")
+    extraAmountInput.setAttribute("hidden", "true");
   }
-})
+});
 
 totalYearsInput.addEventListener("change", () => {
-  totalYears = parseFloat(totalYearsInput.value)
-  totalYearsInputNumber.innerHTML = totalYearsInput.value
-  totalIncuredInterestEvents = interestCalculationsPerYear * totalYears
-})
-
+  totalYears = parseFloat(totalYearsInput.value);
+  totalYearsInputNumber.innerHTML = totalYearsInput.value;
+  totalIncuredInterestEvents = interestCalculationsPerYear * totalYears;
+});
 
 // setup global events
 document.addEventListener("change", () => {
-  compute()
-})
+  compute();
+});
 
 window.addEventListener("load", () => {
   // sync values
-  yearlyInterestRate = parseFloat(yearlyInterestRateInput.value) / 100
-  interestCalculationsPerYear = parseInt(interestPerInput.value)
-  initialAmount = parseFloat(initialAmountInput.value)
-  extra = parseFloat(extraAmountInput.value)
-  totalYears = parseInt(totalYearsInput.value)
-  incuredInterestRate = yearlyInterestRate / interestCalculationsPerYear
-  totalIncuredInterestEvents = interestCalculationsPerYear * totalYears
-  totalYearsInputNumber.innerHTML = `${totalYearsInput.value}`
-  compute()
-})
-
+  yearlyInterestRate = parseFloat(yearlyInterestRateInput.value) / 100;
+  interestCalculationsPerYear = parseInt(interestPerInput.value);
+  initialAmount = parseFloat(initialAmountInput.value);
+  extra = parseFloat(extraAmountInput.value);
+  totalYears = parseInt(totalYearsInput.value);
+  incuredInterestRate = yearlyInterestRate / interestCalculationsPerYear;
+  totalIncuredInterestEvents = interestCalculationsPerYear * totalYears;
+  totalYearsInputNumber.innerHTML = `${totalYearsInput.value}`;
+  compute();
+});
